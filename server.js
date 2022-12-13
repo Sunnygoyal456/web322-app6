b3e4b66eb16ce3d3c11c5d97b2fa6ff01e4b4347
Sunnygoyal456 <Sunnygoyal456.sg@gmail.com>
2022-12-12 23:31:36 -0500

    added comment

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+/********************************************************************************* 
+*  WEB322 – Assignment 6 
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.   
+*  No part of this assignment has been copied manually or electronically from any other source 
+*  (including web sites) or distributed to other students. 
+*  
+*  Name: ____Pratham_Garg_________________ Student ID: __117900217____________ Date: ____12-12-2022____________ 
+* 
+*  Online (Cyclic) URL: ___________https://long-teal-chick-kilt.cyclic.app (this the cyclic link for Asssignment 6)
+
+                        ____https://tan-magnificent-camel.cyclic.app/register (this is the cyclin link for Assignment 5 )________________________________________ 
+* 
+********************************************************************************/
+
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
